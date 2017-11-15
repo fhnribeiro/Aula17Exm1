4eb3d3415724f5edd8a4a3eae1061aefedb62a39
11944413600 <11944413600@lab06m43.nrc.ice.ufjf.br>
2017-11-14 22:45:04 -0200

    Primeiro commit

--- src/aula17/ProdutoDAO.java
+++ src/aula17/ProdutoDAO.java
@@ -6,10 +6,8 @@
 package aula17;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,13 +17,18 @@ import java.util.List;
  */
 public class ProdutoDAO {
     
-    public void criar(Produto prod) throws Exception{
-        Class.forName("org.apache.derby.jdbc.ClientDriver");
-        String driverURL = "jdbc:derby://localhost:1527/2017-3-DCC171";
-        Connection conexao = DriverManager.getConnection(driverURL,"usuario","senha");
+    private Connection conexao;
+    private PreparedStatement operacaoInsere;
+    private PreparedStatement operacaoListar;
 
-        PreparedStatement operacaoInsere = conexao.prepareStatement("INSERT INTO produto(nome,qtd,atualizado) VALUES(?,?,CURRENT_TIMESTAMP)");
-        
+    public ProdutoDAO() throws Exception {
+        conexao = ConexaoJavaDB.getConnection();
+        operacaoInsere = conexao.prepareStatement("INSERT INTO produto(nome,qtd,atualizado) VALUES(?,?,CURRENT_TIMESTAMP)");
+        operacaoListar = conexao.prepareStatement("SELECT nome,qtd FROM produto WHERE qtd>?");
+    }
+    
+    
+    public void criar(Produto prod) throws Exception{
         operacaoInsere.clearParameters();
         operacaoInsere.setString(1, prod.getNome());
         operacaoInsere.setInt(2, prod.getQtd());
@@ -34,11 +37,7 @@ public class ProdutoDAO {
     
     public List<Produto> listarTodos() throws Exception{
         ArrayList<Produto> produtos=null;
-        Class.forName("org.apache.derby.jdbc.ClientDriver");
-        String driverURL = "jdbc:derby://localhost:1527/2017-3-DCC171";
-        Connection conexao = DriverManager.getConnection(driverURL,"usuario","senha");
-
-        PreparedStatement operacaoListar = conexao.prepareStatement("SELECT nome,qtd FROM produto WHERE qtd>?");
+        
         operacaoListar.clearParameters();
         operacaoListar.setInt(1, 0);
         ResultSet rs = operacaoListar.executeQuery();
